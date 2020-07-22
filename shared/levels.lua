3734fb65f4500781e475752ca70ef38fd39e743b
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-07-22 20:15:57 +0100

    Added more levels

--- shared/levels.lua
+++ shared/levels.lua
@@ -6,12 +6,44 @@ Citizen.CreateThread(function()
   KOTH.Levels = {
     [1] = {
       Threshold = 1000,
-      Rank = "Recruit"
+      Rank = "Private"
     },
     [2] = {
       Threshold = 2000,
-      Rank = "Cadet"
-    }
+      Rank = "Private First Class"
+    },
+    [3] = {
+      Threshold = 3000,
+      Rank = "Specialist"
+    },
+    [4] = {
+      Threshold = 4000,
+      Rank = "Corporal"
+    },
+    [5] = {
+      Threshold = 5000,
+      Rank = "Sergeant"
+    },
+    [6] = {
+      Threshold = 6000,
+      Rank = "Staff Sergeant"
+    },
+    [7] = {
+      Threshold = 7000,
+      Rank = "Sergeant First Class"
+    },
+    [8] = {
+      Threshold = 8000,
+      Rank = "Master Sergeant"
+    },
+    [9] = {
+      Threshold = 9000,
+      Rank = "First Sergeant"
+    },
+    [10] = {
+      Threshold = 1000000,
+      Rank = "Sergeant Major"
+    },
   }
-  
+
 end)
