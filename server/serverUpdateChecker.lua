b74710ad407c2d2f2fec81ef633ef74e3d350e9c
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-07-23 01:40:59 +0100

    Add prefixes to UpdateChecker

--- server/serverUpdateChecker.lua
+++ server/serverUpdateChecker.lua
@@ -2,20 +2,20 @@ KOTH.VersionFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
 KOTH.Version = json.decode(KOTH.VersionFile).version
 
 PerformHttpRequest("https://raw.githubusercontent.com/The-Neco/KOTH/master/version.json", function (errorCode, resultData, resultHeaders)
-  print("^1==================================================================================\n^7Checking if ^5KOTH^7 is up to date")
+  print("^1==================================================================================\n^7[^5KOTH^7] Checking if ^5KOTH^7 is up to date")
   if resultData ~= nil then
     local data = json.decode(resultData)
     if KOTH.Version ~= data.version and KOTH.Version < data.version then
-      print("^8WARNING: ^5KOTH ^7is not up to date.\nYou are currently using ^8" .. KOTH.Version .. "^7 the latest version is ^2" .. data.version .. ".\n^7Please download the latest version from https://github.com/The-Neco/KOTH/releases")
+      print("[^5KOTH^7] ^8WARNING: ^5KOTH ^7is not up to date.\nYou are currently using ^8" .. KOTH.Version .. "^7 the latest version is ^2" .. data.version .. ".\n^7Please download the latest version from https://github.com/The-Neco/KOTH/releases")
     else
       if KOTH.Version > data.version then
-        print("^8WARNING: ^7You are using a pre-release version of ^5KOTH ^7there may be bugs.")
+        print("[^5KOTH^7] ^8WARNING: ^7You are using a pre-release version of ^5KOTH ^7there may be bugs.")
       else
-        print("^5KOTH ^7is up to date.")
+        print("[^5KOTH^7] ^5KOTH ^7is up to date.")
       end
     end
   else
-    print("^7Failed to check if ^5KOTH ^7is up to date.")
+    print("[^5KOTH^7] ^7Failed to check if ^5KOTH ^7is up to date.")
   end
   print("^1==================================================================================^7")
 end)
