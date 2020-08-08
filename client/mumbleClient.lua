906872d8e8b51ae82b5ba1368527680ebeee0f90
Lewis <13007790+The-Neco@users.noreply.github.com>
2020-08-09 00:15:41 +0100

    Feature (mumble filters): Fucking big brained the fuck outta mumble.

--- client/mumbleClient.lua
+++ client/mumbleClient.lua
@@ -78,13 +78,17 @@ Citizen.CreateThread(function()
         if Distance <= KOTH.Mumble.Config.Range then
           if VoiceData.IsTalking and VoiceData.Channel == LocalData.Channel then
             PlayersToHear[ServerID] = 1.0
+            KOTH.Mumble.ToggleRadioEffect(ServerID, true)
           else
+            KOTH.Mumble.ToggleRadioEffect(ServerID, false)
             PlayersToHear[ServerID] = round(1.0 - (Distance / KOTH.Mumble.Config.Range) + 0.0001, 2)
           end
         else
           if VoiceData.IsTalking and VoiceData.Channel == LocalData.Channel then
             PlayersToHear[ServerID] = 1.0
+            KOTH.Mumble.ToggleRadioEffect(ServerID, true)
           else
+            KOTH.Mumble.ToggleRadioEffect(ServerID, false)
             PlayersToHear[ServerID] = 0.0
           end
         end
@@ -93,118 +97,53 @@ Citizen.CreateThread(function()
 
     --[[ NOW LETS ACTUALLY HEAR THOSE PLAYERS ]]
     for ServerID, Volume in pairs(PlayersToHear) do
-      MumbleSetVolumeOverride(GetPlayerFromServerId(ServerID), Volume)
+      MumbleSetVolumeOverrideByServerId(ServerID, Volume)
     end
 
   end
 end)
 
---[[ MUMBLE RADIO DISTORTION ]]
-
-KOTH.Mumble.GetPlayerAudioContext = function(PlayerServerID)
-  return n.GET_AUDIOCONTEXT_FOR_CLIENT(GetPlayerFromServerId(PlayerID), Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-end
-
-KOTH.Mumble.GetSourceDestination = function(PlayerAudioContext)
-  local source = n.AUDIOCONTEXT_GET_SOURCE(PlayerAudioContext, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-  local destination = n.AUDIOCONTEXT_GET_DESTINATION(PlayerAudioContext, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-  return source, destination
-end
-
---[[
-KOTH.Mumble.AudioContextConnect(PlayerAudioContext, Source, Destination, [OPTIONAL]Filter)
-CONNECTS NODES
-
-EXAMPLES:
-
-KOTH.Mumble.AudioContextConnect(PlayerAudioContext, source, destination) - Connects the default nodes removing all filters, must be called after clearning the filters..
-KOTH.Mumble.AudioContextDisconnect(PlayerAudioContext, source, destination, filter) - Enables that specific filter.
-]]
-
-
-KOTH.Mumble.AudioContextConnect = function(PlayerAudioContext, Source, Destination, Filter)
-  if Filter == nil then
-    n.AUDIOCONTEXT_CONNECT(PlayerAudioContext, Destination, Source)
-  else
-    n.AUDIOCONTEXT_CONNECT(PlayerAudioContext, Filter, Source)
-    n.AUDIOCONTEXT_CONNECT(PlayerAudioContext, Destination, Filter)
-  end
-end
-
---[[
-KOTH.Mumble.AudioContextDisconnect(PlayerAudioContext, Node1, Node2)
-DISCONNECTS NODES
-
-EXAMPLES:
-
-KOTH.Mumble.AudioContextDisconnect(PlayerAudioContext, source, destination) - Disconnects default nodes making it ready for filters to be added.
-KOTH.Mumble.AudioContextDisconnect(PlayerAudioContext, destination, filter) - Disables that specific filter.
-]]
-
-
-KOTH.Mumble.AudioContextDisconnect = function(PlayerAudioContext, Node1, Node2)
-  n.AUDIOCONTEXT_DISCONNECT(PlayerAudioContext, Node1, Node2)
-end
-
-KOTH.Mumble.CreateBiquadFilter = function(PlayerAudioContext, Settings)
-  local bfn = n.AUDIOCONTEXT_CREATE_BIQUADFILTERNODE(PlayerAudioContext, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-  if Settings.Type ~= nil then
-    n.BIQUADFILTERNODE_SET_TYPE(bfn, Settings.Type)
-  end
-  if Settings.Freq ~= nil then
-    n.BIQUADFILTERNODE_FREQUENCY(bfn, Settings.Freq)
-  end
-  if Settings.Q ~= nil then
-    n.BIQUADFILTERNODE_Q(bfn, Settings.Q)
-  end
-  if Settings.Gain ~= nil then
-    n.BIQUADFILTERNODE_GAIN(bfn, Settings.Gain)
-  end
-  if Settings.Detune ~= nil then
-    n.BIQUADFILTERNODE_DETUNE(bfn, Settings.Detune)
-  end
-  return bfn
-end
-
-KOTH.Mumble.DestroyBiquadFilter = function(BFN)
-  n.BIQUADFILTERNODE_DESTROY(BFN)
-  return nil
-end
-
-KOTH.Mumble.CreateWaveshaper = function(PlayerAudioContext, WaveAmount)
-  local curve = distortionCurve(WaveAmount) -- just any function that outputs an array of numbers to describe distortion
-  local wsn = n.AUDIOCONTEXT_CREATE_WAVESHAPERNODE(PlayerAudioContext, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-  n.WAVESHAPERNODE_SET_CURVE(wsn, makeObject(curve))
-  return wsn
-end
-
-KOTH.Mumble.DestroyWaveshaper = function(WSN)
-  n.WAVESHAPERNODE_DESTROY(WSN)
-  return nil
-end
-
 KOTH.Mumble.ToggleRadioEffect = function(player, bool)
   if KOTH.Mumble.ClientData[player] == nil then
     KOTH.Mumble.ClientData[player] = {}
   end
-  if bool and KOTH.Mumble.ClientData[player].WSN == nil then
-    KOTH.Mumble.ClientData[player].Context = KOTH.Mumble.GetPlayerAudioContext(player) -- GETS PLAYERS AUDIO CONTEXT
-    KOTH.Mumble.ClientData[player].Source, KOTH.Mumble.ClientData[player].Destination = KOTH.Mumble.GetSourceDestination(KOTH.Mumble.ClientData[player].Context) -- GETS PLAYERS SOURCE AND DESTINATION
-    KOTH.Mumble.AudioContextDisconnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].Source) -- DISCONNECTS DEFAULT NODES
-    KOTH.Mumble.ClientData[player].WSN = KOTH.Mumble.CreateWaveshaper(KOTH.Mumble.ClientData[player].Context, 1) -- CREATES WSN NODE
-    KOTH.Mumble.ClientData[player].BFN = KOTH.Mumble.CreateBiquadFilter(KOTH.Mumble.ClientData[player].Context, {Type = "lowpass", Freq = 10000.0, Q = 0.707, Gain = 1.0}) -- CREATES BFN NODE
-    KOTH.Mumble.AudioContextConnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Source, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].WSN) -- CONNECTS WSN NODE
-    KOTH.Mumble.AudioContextConnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Source, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].BFN) -- CONNECTS BFN NODE
-  elseif not bool and KOTH.Mumble.ClientData[player].WSN ~= nil then
-    KOTH.Mumble.AudioContextDisconnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].WSN) -- REMOVES WSN NODE
-    KOTH.Mumble.AudioContextDisconnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].BFN) -- REMOVES BFN NODE
-    KOTH.Mumble.AudioContextConnect(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Source, KOTH.Mumble.ClientData[player].Destination) -- SETS DEFAULT NODES
-    KOTH.Mumble.ClientData[player].WSN = KOTH.Mumble.DestroyWaveshaper(WSN)
-    KOTH.Mumble.ClientData[player].BFN = KOTH.Mumble.DestroyBiquadFilter(BFN)
+  if bool and KOTH.Mumble.ClientData[player].BFN == nil then
+    local p = GetPlayerFromServerId(player)
+    if p == -1 then
+      return
+    end
+    KOTH.Mumble.ClientData[player].Context = n.GET_AUDIOCONTEXT_FOR_CLIENT(p, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    KOTH.Mumble.ClientData[player].Source = n.AUDIOCONTEXT_GET_SOURCE(KOTH.Mumble.ClientData[player].Context, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    KOTH.Mumble.ClientData[player].Destination = n.AUDIOCONTEXT_GET_DESTINATION(KOTH.Mumble.ClientData[player].Context, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    local curve = distortionCurve(20) -- just any function that outputs an array of numbers to describe distortion
+    local wsn = n.AUDIOCONTEXT_CREATE_WAVESHAPERNODE(KOTH.Mumble.ClientData[player].Context, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    n.WAVESHAPERNODE_SET_CURVE(wsn, makeObject(curve))
+    bfn = n.AUDIOCONTEXT_CREATE_BIQUADFILTERNODE(KOTH.Mumble.ClientData[player].Context, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    n.BIQUADFILTERNODE_SET_TYPE(bfn, "lowshelf")
+    n.BIQUADFILTERNODE_FREQUENCY(bfn, 1000.0)
+    local f = n.BIQUADFILTERNODE_GAIN(bfn, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    n.AUDIOPARAM_SET_VALUE(f, -50.0)
+    KOTH.Mumble.ClientData[player].BFN = n.AUDIOCONTEXT_CREATE_BIQUADFILTERNODE(KOTH.Mumble.ClientData[player].Context, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
+    n.BIQUADFILTERNODE_SET_TYPE(KOTH.Mumble.ClientData[player].BFN, "highpass")
+    n.BIQUADFILTERNODE_FREQUENCY(KOTH.Mumble.ClientData[player].BFN, 24000.0)
+    n.BIQUADFILTERNODE_GAIN(KOTH.Mumble.ClientData[player].BFN, 2150.0)
+
+    n.AUDIOCONTEXT_CONNECT(KOTH.Mumble.ClientData[player].Context, wsn, KOTH.Mumble.ClientData[player].Source, 0, 0)
+    Citizen.Wait(1)
+    n.AUDIOCONTEXT_CONNECT(KOTH.Mumble.ClientData[player].Context, bfn, wsn, 0, 0)
+    Citizen.Wait(1)
+    n.AUDIOCONTEXT_CONNECT(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].BFN, bfn, 0, 0)
+    Citizen.Wait(1)
+    n.AUDIOCONTEXT_CONNECT(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].BFN, 0, 0)
+  elseif not bool and KOTH.Mumble.ClientData[player].BFN ~= nil then
+    n.AUDIOCONTEXT_DISCONNECT(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].BFN, 0, 0)
+    Citizen.Wait(1)
+    n.AUDIOCONTEXT_CONNECT(KOTH.Mumble.ClientData[player].Context, KOTH.Mumble.ClientData[player].Destination, KOTH.Mumble.ClientData[player].Source, 0, 0)
+    KOTH.Mumble.ClientData[player].BFN = nil
   end
 end
 
-local n = setmetatable({}, {
+n = setmetatable({}, {
     __index = function(t, k)
         return function(...)
             return Citizen.InvokeNative(GetHashKey(k) & 0xFFFFFFFF, ...)
@@ -217,7 +156,7 @@ function distortionCurve(k)
 
     local deg = math.pi / 180
 
-    for i = 1, 40000 do
+    for i = 1, 42000 do
         local x = (i - 1) * 2 / 48000 - 1;
         curve[i] = ( 3 + k ) * x * 20 * deg / ( math.pi + k * math.abs(x) );
     end
@@ -229,43 +168,3 @@ function makeObject(data)
 
     return string.pack('<T', #d) .. d
 end
-
-Citizen.CreateThread(function()
-  Citizen.Wait(5000)
-  for ServerID, VoiceData in pairs(KOTH.Mumble.Players) do
-    Citizen.CreateThread(function()
-      print(ServerID)
-      local p = GetPlayerFromServerId(ServerID)
-      local r = n.GET_AUDIOCONTEXT_FOR_CLIENT(p, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-
-      -- original source and destination nodes
-      local s = n.AUDIOCONTEXT_GET_SOURCE(r, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-      local d = n.AUDIOCONTEXT_GET_DESTINATION(r, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-
-      n.AUDIOCONTEXT_DISCONNECT(r, d, s, 0, 0) -- disconnect the source from its destination so we can add a wave shaper node
-
-      local curve = distortionCurve(1) -- just any function that outputs an array of numbers to describe distortion
-      wsn = n.AUDIOCONTEXT_CREATE_WAVESHAPERNODE(r, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-      n.WAVESHAPERNODE_SET_CURVE(wsn, makeObject(curve))
-      bfn = n.AUDIOCONTEXT_CREATE_BIQUADFILTERNODE(r, Citizen.ResultAsLong(), Citizen.ReturnResultAnyway())
-      n.BIQUADFILTERNODE_SET_TYPE(bfn, "lowpass")
-      n.BIQUADFILTERNODE_FREQUENCY(bfn, 10000.0)
-      n.BIQUADFILTERNODE_Q(bfn, 0.707)
-      n.BIQUADFILTERNODE_GAIN(bfn, 1.0)
-
-      n.AUDIOCONTEXT_CONNECT(r, wsn, s, 0, 0) -- connect our new node to source
-      n.AUDIOCONTEXT_CONNECT(r, d, wsn, 0, 0) -- connect the destination node to our new node
-      n.AUDIOCONTEXT_CONNECT(r, bfn, s, 0, 0) -- connect our new node to source
-      n.AUDIOCONTEXT_CONNECT(r, d, bfn, 0, 0) -- connect the destination node to our new node
-
-      -- now distorted hooray!
-      print("Filter set")
-
-      Citizen.Wait(10000)
-      n.AUDIOCONTEXT_DISCONNECT(r, d, bfn, 0, 0) -- disconnect the WaveShaperNode
-      n.AUDIOCONTEXT_DISCONNECT(r, d, wsn, 0, 0) -- disconnect the WaveShaperNode
-      n.AUDIOCONTEXT_CONNECT(r, d, s, 0, 0) -- reconnect back to the original source node
-      print("Filter unset")
-    end)
-  end
-end)
